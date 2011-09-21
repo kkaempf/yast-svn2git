a669329890c225b8849704671167941565cba593
Klaus Kämpf <kkaempf@suse.de>
2011-09-21 13:47:24 +0200

    Add usage to svnerrors.sh

--- svnerrors.sh
+++ svnerrors.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#
+# Find failed svn imports by looking for non-empty stderr.svnadmin files
+#
 for i in `find . -name stderr.svnadmin -size +0`; do
   j=`dirname $i`
   j=`basename $j .dir`
